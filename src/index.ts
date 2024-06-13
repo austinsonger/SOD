ef55041bf6415568ee2d73560e4dffacb5394961
Austin Songer,MIS,CEH,ESCA,Project+ (Navy Veteran) <a.songer@protonmail.com>
2024-06-12 23:18:55 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,9 @@ import { getCommitters, getApprovers, requestReviewer, parseCodeownersFile, find
 import { Octokit } from "@octokit/core";
 import { paginateRest } from "@octokit/plugin-paginate-rest";
 import { restEndpointMethods } from "@octokit/plugin-rest-endpoint-methods";
+import { Api } from "@octokit/plugin-rest-endpoint-methods/dist-types/types";
 
-type ExtendedOctokit = Octokit & { paginate: typeof paginateRest } & { rest: ReturnType<typeof restEndpointMethods> };
+type ExtendedOctokit = Octokit & { paginate: typeof paginateRest; rest: ReturnType<typeof restEndpointMethods> };
 
 const MyOctokit = Octokit.plugin(paginateRest, restEndpointMethods);
 
@@ -13,34 +14,34 @@ async function run() {
   try {
     const token = core.getInput('github-token');
     const codeownersFile = core.getInput('codeowners-file');
-    
-    const octokit = new MyOctokit({ auth: token }) as ExtendedOctokit;
+
+    const octokit = new MyOctokit({ auth: token });
     const context = github.context;
     const pullRequest = context.payload.pull_request;
-    
+
     if (!pullRequest) {
       core.setFailed('This action can only be run in the context of a pull request.');
       return;
     }
-    
-    const committers = await getCommitters(octokit, context.repo.owner, context.repo.repo, pullRequest.number);
+
+    const committers = await getCommitters(octokit as ExtendedOctokit, context.repo.owner, context.repo.repo, pullRequest.number);
     const approvers = getApprovers(pullRequest);
     const ineligibleApprovers = approvers.filter(approver => committers.has(approver));
-    
+
     if (ineligibleApprovers.length > 0) {
       core.info(`Ineligible approvers: ${ineligibleApprovers.join(', ')}`);
     }
-    
+
     const codeowners = await parseCodeownersFile(codeownersFile);
     const eligibleApprover = findEligibleApprover(codeowners, committers);
-    
+
     if (!eligibleApprover) {
       core.setFailed('No eligible approver found.');
       return;
     }
-    
-    await requestReviewer(octokit, context.repo.owner, context.repo.repo, pullRequest.number, eligibleApprover);
-    
+
+    await requestReviewer(octokit as ExtendedOctokit, context.repo.owner, context.repo.repo, pullRequest.number, eligibleApprover);
+
     core.info(`Assigned new approver: @${eligibleApprover}`);
   } catch (error) {
     if (error instanceof Error) {
